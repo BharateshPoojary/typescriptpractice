a28046a764dc4a2873eb4111a55827a55e43e1ca
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-26 18:00:02 +0530

    enums in ts

---
+++ Enum.ts
@@ -0,0 +1,7 @@
+enum seatChoice{
+    FIRST =1,
+    SECOND,
+    THIRD,
+    FOURTH
+}
+const seatchoice = seatChoice.FIRST;//so here  seatChoice.FIRST will return  1 if not mentioned then default value will be returned that is 0
