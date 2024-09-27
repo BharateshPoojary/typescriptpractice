01b83dcd248dd30092a3d8f56d96eb43b3608dfd
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-27 17:09:59 +0530

    Class in ts

---
+++ Class.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = /** @class */ (function () {
+    function User(uname, id) {
+        this.name = uname;
+        this.Id = id;
+    }
+    return User;
+}());
+var printusercreds = new User("bharat", 26);
+printusercreds.city = "";
