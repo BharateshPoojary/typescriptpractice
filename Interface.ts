b8a6fd68b71e88feb35d5d29b1c1d0a792284377
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-27 12:09:59 +0530

    interface vs type aliases

--- Interface.ts
+++ Interface.ts
@@ -1,4 +1,4 @@
-interface Userint{
+interface A{
     readonly dbId : number
     name:string
     DiscId?:number
@@ -9,8 +9,19 @@ interface Userint{
     generateDisct(couponname:string,discount:number):number | string
 //generateSisct() includes parameter and is returning either string or number
 }
-const user :Userint={
+
+//REOPENING OF AN INTERFACE
+interface A{//This cannot be done in type aliases it leads to an error
+    Uid:number//adding new field to an extending interface 
+}
+interface B extends A{//We can extend the interface like this using extends keyword
+Bsid:number//we can add more field to an existing interface like this or else in type aliases we have to using & symbol
+}
+
+const user :B={
+    Bsid : 17,
     dbId:23,
+    Uid:12,
     name:"bharat",
     generateDisct:(cname = "dhamaka",discount =10)=> {//The parameter name can be different here no issue 
         return discount
