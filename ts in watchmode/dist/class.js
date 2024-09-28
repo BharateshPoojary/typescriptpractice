fa07927306f1685f37d2afff41b0d64b1638e250
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-09-28 11:53:11 +0530

    private and public in ts

--- ts in watchmode/dist/class.js
+++ ts in watchmode/dist/class.js
@@ -1,12 +1,36 @@
 "use strict";
+//This is METHOD 1
+// class User{
+//     private name:string;
+//     public  id:number;//bydefault it is public only 
+//     readonly city:string;//declaring types for fields
+//     constructor( 
+//       uname:string,id:number,ucity:string ){//constructor with ucity,uname as string and id as number 
+//         this.name = uname;
+//         this.id = id;
+//         this.city = ucity;
+//     }
+// getName(){
+//      return this.name;//name can only be accessed with in class
+// }
+// }
+//METHOD 2 USED BY EXPERTS
 class User {
-    constructor(uname, id) {
-        this.city = ""; //declaring types for fields
-        this.name = uname;
-        this.Id = id;
+    constructor(//This is a simplified syntax in order to avoid this. keyword inside parameter only we have to use modifers and it is automatically assigned as this.name = name
+    name, id, city) {
+        this.name = name;
+        this.id = id;
+        this.city = city;
+    }
+    getName() {
+        return this.name; //name can only be accessed with in class
     }
 }
-const printusercreds = new User("bharat", 26); //"bharat" is unamw and 26 is id here it is passed to name and Id of user class
+const printusercreds = new User("bharat", 26, "Mumbai"); //"bharat" is unamw and 26 is id here it is passed to name and Id of user class
+console.log(printusercreds.getName()); //This will return name value
 //we can access the fields now like name and Id
-console.log(printusercreds.name);
-printusercreds.city = "Mumbai";
+//printusercreds.city = "Mumbai";we cannot assign like this as it is readonly string
+// console.log(printusercreds.name);this cannot be accessed as it is in private scope can be accessed within class itself 
+//by default all fields are in public scope it is not necessary to explicitly mention it using keyword but yeah its a good practice
+/**private: Warns if it's not used within the class.
+public: No warning, as it could be accessed outside the class. */ 
--- ts in watchmode/src/class.ts
+++ ts in watchmode/src/class.ts
@@ -1,14 +1,41 @@
-class User{
-    name:string;
-    Id:number;
-    city:string="";//declaring types for fields
-    constructor(uname:string,id:number){//constructor with uname as string and id as number 
-        this.name = uname;
-        this.Id=id;
-    }
-}
-const printusercreds = new User("bharat",26);//"bharat" is unamw and 26 is id here it is passed to name and Id of user class
-//we can access the fields now like name and Id
-console.log(printusercreds.name);
 
-printusercreds.city = "Mumbai";
+//This is METHOD 1
+// class User{
+//     private name:string;
+//     public  id:number;//bydefault it is public only 
+//     readonly city:string;//declaring types for fields
+//     constructor( 
+//       uname:string,id:number,ucity:string ){//constructor with ucity,uname as string and id as number 
+//         this.name = uname;
+//         this.id = id;
+//         this.city = ucity;
+       
+//     }
+// getName(){
+//      return this.name;//name can only be accessed with in class
+// }
+   
+// }
+//METHOD 2 USED BY EXPERTS
+class User{
+  
+    constructor( //This is a simplified syntax in order to avoid this. keyword inside parameter only we have to use modifers and it is automatically assigned as this.name = name
+      private name:string,
+      public id:number,
+      public city:string  ){//here inside parantheses nothing we have to mention assignment is automatically done
+    }
+getName(){
+     return this.name;//name can only be accessed with in class
+}
+   
+}
+const printusercreds = new User("bharat",26,"Mumbai");//"bharat" is unamw and 26 is id here it is passed to name and Id of user class
+console.log(printusercreds.getName());//This will return name value
+
+//we can access the fields now like name and Id
+//printusercreds.city = "Mumbai";we cannot assign like this as it is readonly string
+// console.log(printusercreds.name);this cannot be accessed as it is in private scope can be accessed within class itself 
+//by default all fields are in public scope it is not necessary to explicitly mention it using keyword but yeah its a good practice
+
+/**private: Warns if it's not used within the class.
+public: No warning, as it could be accessed outside the class. */
