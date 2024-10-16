546c4b17e6d2a0ae3546ca0e93ec4779fa14bb27
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-10-16 21:58:29 +0530

    in operator narrowing in ts

--- ts in watchmode/src/Typenarrow.ts
+++ ts in watchmode/src/Typenarrow.ts
@@ -8,10 +8,17 @@ function typeNarrow(a :string|number|null|string[]){
         if(typeof a === "string"){
             return a.toLowerCase();
         }else if(typeof a === "object"){
-            a.forEach(asele=>{
-                console.log(asele);
-            })
+            // a.forEach(asele=>{
+            //     console.log(asele);
+            // })
+            // return
+            for(const s of a){
+                console.log(s);
+                
+            }
             return
+            //foreach and for..of loop is specifically for an array
+            //for..in loop can be used for objects of keyvalue pair
         }
         return a+3;
     }else{
@@ -20,3 +27,20 @@ function typeNarrow(a :string|number|null|string[]){
     }  
 }
 //here we have made type narrowing where we used typeof operator in order to check the type and perform  specific type operation 
+//the in operator narrowing
+interface Creditors{
+    acname:string,
+    acno:number
+}
+interface Debtors{
+    acname:string,
+    acno:number,
+    isPaid:boolean
+}
+function isDebtPaid(client:Creditors|Debtors){
+    //return client.isPaid It will throw an error as property isPaid only exists with Debtors interface
+    if("isPaid" in client){
+        return client.isPaid;
+    }//here we used the in operator to breakdown the properties of interface and we checked if ispaid property exist in client if it is then inside that we can access the isPaid property(Insideif client will be debtors only as we checked using in operator if the property exists then only it will come else it will not  
+
+}
