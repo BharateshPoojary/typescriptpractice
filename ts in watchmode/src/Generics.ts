e7cb8fb74bc91913bf16d279d007dd14c26bc9ba
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-10-16 11:14:22 +0530

    using arrow functions and array in functions

--- ts in watchmode/src/Generics.ts
+++ ts in watchmode/src/Generics.ts
@@ -9,7 +9,7 @@ return val ;
 function secondOne<Type>(val:Type):Type{
 return val;
 }
-secondOne("5")//here as we used generic type it can accept value of any type and it locks the particular type and returns in that type only 
+secondOne(3)//here as we used generic type it can accept value of any type and it locks the particular type and returns in that type only 
 //we can define a custom type as well
 interface customType{
     brand:string,
@@ -25,3 +25,15 @@ Using secondOne with <customType>: When you call secondOne<customType>, you’re
 This means secondOne now expects an object that matches the structure of customType and will return the same type.
 So, { brand: "3", type: 7 } is passed in as the argument, which satisfies the customType structure, and the function returns this object with the exact same type.
  */
+//we can pass an array to function secondOne([1,2,3]) like this and the function type is automatically converted to array type so there is a question 
+//why to Use Type[] Explicitly in function definition ? the reason is If you explicitly define the parameter as Type[], you’re making it clear that the function is designed to only accept arrays.
+//it will be benficial for other devs to understand the code
+function thirdOne<T>(myarr:T[]):T[]{
+return myarr;
+}
+//here I have defined a function  which takes an array as input parameter and returns an array itself
+
+//Using arrow functions <T,> this is juat a syntactical sugar where we put , inside <T> in order to differentiate between a react component and generic type
+const fourthOne =<T,> (val:T[]):T=>{
+return val[4];
+}
